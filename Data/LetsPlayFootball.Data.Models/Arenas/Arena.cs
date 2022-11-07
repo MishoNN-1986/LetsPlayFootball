39a2ae7f7b7235b5101764a1ba339960c6da965b
Mihail <mishocara@gmail.com>
2022-11-07 18:22:23 +0200

    Add Arena Status Enumeration and Arena DbModel

---
+++ Data/LetsPlayFootball.Data.Models/Arenas/Arena.cs
@@ -0,0 +1,33 @@
+﻿namespace LetsPlayFootball.Data.Models.Arenas
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using LetsPlayFootball.Data.Common.Models;
+
+    public class Arena : BaseDeletableModel<int>
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(15)]
+        public string PhoneNumber { get; set; }
+
+        [MaxLength(260)]
+        public string WebUrl { get; set; }
+
+        [MaxLength(50)]
+        public string Email { get; set; }
+
+        [MaxLength(2000)]
+        public string Description { get; set; }
+
+
+    }
+}
---
+++ Data/LetsPlayFootball.Data.Models/Arenas/ArenaStatus.cs
@@ -0,0 +1,20 @@
+﻿namespace LetsPlayFootball.Data.Models.Arenas
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public enum ArenaStatus
+    {
+        Active = 1,
+        Inactive = 2,
+
+        [Display(Name = "In Reconstruction")]
+        InReconstruction = 3,
+
+        [Display(Name = "Temporary Unavailable")]
+        TemporatyUnavailable = 4,
+
+        [Display(Name = "Opening Soon")]
+        OpeningSoon = 5,
+        Closed = 6,
+    }
+}
