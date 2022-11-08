127fd0b20bbeea8f3da054125bd767d6cc18ff12
Mihail <mishocara@gmail.com>
2022-11-08 20:01:07 +0200

    Add Address and City Db Models

---
+++ Data/LetsPlayFootball.Data.Models/Address.cs
@@ -0,0 +1,23 @@
+﻿namespace LetsPlayFootball.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using LetsPlayFootball.Data.Common.Models;
+    using LetsPlayFootball.Data.Models.Arenas;
+
+    public class Address : BaseDeletableModel<int>
+    {
+        [MaxLength(100)]
+        public string AddressInformation { get; set; }
+
+        [Required]
+        public int ArenaId { get; set; }
+
+        public virtual Arena Arena { get; set; }
+
+        [Required]
+        public int CityId { get; set; }
+
+        public virtual City City { get; set; }
+    }
+}
---
+++ Data/LetsPlayFootball.Data.Models/City.cs
@@ -0,0 +1,16 @@
+﻿namespace LetsPlayFootball.Data.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    using LetsPlayFootball.Data.Common.Models;
+
+    public class City : BaseDeletableModel<int>
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        public virtual ICollection<Address> Addreses { get; set; } = new HashSet<Address>();
+    }
+}
