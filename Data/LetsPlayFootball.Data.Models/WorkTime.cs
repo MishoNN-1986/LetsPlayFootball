db8a38bdc21a2199a689cc3b68e9a49cf93a4566
Mihail <mishocara@gmail.com>
2022-11-09 13:59:12 +0200

    Add WorkTime Db Model

---
+++ Data/LetsPlayFootball.Data.Models/WorkTime.cs
@@ -0,0 +1,25 @@
+﻿namespace LetsPlayFootball.Data.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    using LetsPlayFootball.Data.Common.Models;
+    using LetsPlayFootball.Data.Models.Arenas;
+
+    public class WorkTime : BaseModel<int>
+    {
+        [Required]
+        public DayOfWeek Day { get; }
+
+        [Required]
+        public string StartTime { get; set; }
+
+        [Required]
+        public string EndTime { get; set; }
+
+        [Required]
+        public int ArenaId { get; set; }
+
+        public Arena Arena { get; set; }
+    }
+}
