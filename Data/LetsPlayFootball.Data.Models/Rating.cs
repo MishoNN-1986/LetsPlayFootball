27906f15ec46e6409833038c03dd85e51e34095b
Mihail <mishocara@gmail.com>
2022-11-09 18:30:55 +0200

    Add Rating Db Model

---
+++ Data/LetsPlayFootball.Data.Models/Rating.cs
@@ -0,0 +1,30 @@
+﻿namespace LetsPlayFootball.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using LetsPlayFootball.Data.Common.Models;
+    using LetsPlayFootball.Data.Models.Arenas;
+
+    public class Rating : BaseModel<int>
+    {
+        [Required]
+        [Range(typeof(int), "0", "5")]
+        public int RatingValue { get; set; }
+
+        [MaxLength(1000)]
+        public string Comment { get; set; }
+
+        [Required]
+        public int AuthorId { get; set; }
+
+        public virtual ApplicationUser Author { get; set; }
+
+        public int? UserId { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+
+        public int? ArenaId { get; set; }
+
+        public virtual Arena Arena { get; set; }
+    }
+}
