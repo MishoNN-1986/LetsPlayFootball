4878a9b7dd6a25f0c27ce87f930a28ebeaf959f5
Mihail <mishocara@gmail.com>
2022-11-09 14:41:49 +0200

    Add Image Db Model

---
+++ Data/LetsPlayFootball.Data.Models/Image.cs
@@ -0,0 +1,27 @@
+﻿namespace LetsPlayFootball.Data.Models
+{
+    using System;
+
+    using LetsPlayFootball.Data.Common.Models;
+    using LetsPlayFootball.Data.Models.Arenas;
+
+    public class Image : BaseDeletableModel<string>
+    {
+        public Image()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        public string Url { get; set; }
+
+        // public string ImageContent { get; set; }
+
+        public int? ArenaId { get; set; }
+
+        public virtual Arena Arena { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+
+        // TODO Add Field
+    }
+}
