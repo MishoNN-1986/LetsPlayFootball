026204802ea42f0efe8838a8988429d63a789311
Mihail <mishocara@gmail.com>
2022-11-10 21:13:34 +0200

    Add Features Db Model

---
+++ Data/LetsPlayFootball.Data.Models/Features.cs
@@ -0,0 +1,23 @@
+﻿namespace LetsPlayFootball.Data.Models
+{
+    using LetsPlayFootball.Data.Common.Models;
+
+    public class Features : BaseModel<int>
+    {
+        public bool Ball { get; set; }
+
+        public bool Jerseys { get; set; }
+
+        public bool Floodlights { get; set; }
+
+        public bool Changeroom { get; set; }
+
+        public bool BathAndToilet { get; set; }
+
+        public bool Parking { get; set; }
+
+        public bool Bench { get; set; }
+
+        // TODO add navigation properties
+    }
+}
