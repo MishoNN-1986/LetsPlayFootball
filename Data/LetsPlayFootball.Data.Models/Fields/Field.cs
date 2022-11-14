7d453078b346045bd8647bd8d3bc5c03ff5701be
Mihail <mishocara@gmail.com>
2022-11-14 11:02:26 +0200

    Add Field Db Model

---
+++ Data/LetsPlayFootball.Data.Models/Fields/Field.cs
@@ -0,0 +1,24 @@
+﻿namespace LetsPlayFootball.Data.Models.Fields
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using LetsPlayFootball.Data.Common.Models;
+    using LetsPlayFootball.Data.Models.Arenas;
+
+    public class Field : BaseDeletableModel<int>
+    {
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(2000)]
+        public string PriceDetails { get; set; }
+
+        [MaxLength(2000)]
+        public string Description { get; set; }
+
+        public int ArenaId { get; set; }
+
+        public virtual Arena Arena { get; set; }
+    }
+}
