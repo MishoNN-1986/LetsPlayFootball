e840c26439576d592345f1b480bc5ffbdbbcae06
Mihail <mishocara@gmail.com>
2022-11-14 13:32:35 +0200

    Update Field Db Model

--- Data/LetsPlayFootball.Data.Models/Fields/Field.cs
+++ Data/LetsPlayFootball.Data.Models/Fields/Field.cs
@@ -1,5 +1,6 @@
 ﻿namespace LetsPlayFootball.Data.Models.Fields
 {
+    using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
 
     using LetsPlayFootball.Data.Common.Models;
@@ -17,8 +18,27 @@
         [MaxLength(2000)]
         public string Description { get; set; }
 
+        [Required]
+        public GameFormat GameFormat { get; set; }
+
+        public FieldType FieldType { get; set; }
+
+        public SurfaceType SurfaceType { get; set; }
+
+        public FieldStatus Status { get; set; }
+
         public int ArenaId { get; set; }
 
         public virtual Arena Arena { get; set; }
+
+        public int SizeId { get; set; }
+
+        public virtual FieldSize Size { get; set; }
+
+        public int FeaturesId { get; set; }
+
+        public virtual Features Features { get; set; }
+
+        public virtual ICollection<Image> Images { get; set; } = new HashSet<Image>();
     }
 }
