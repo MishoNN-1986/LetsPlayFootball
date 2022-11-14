b7fdc041a01fce17e4601af4cca182b7737fe48c
Mihail <mishocara@gmail.com>
2022-11-14 13:04:41 +0200

    Add GameFormat Enumeration

---
+++ Data/LetsPlayFootball.Data.Models/Fields/GameFormat.cs
@@ -0,0 +1,30 @@
+﻿namespace LetsPlayFootball.Data.Models.Fields
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public enum GameFormat
+    {
+        [Display(Name = "5/5")]
+        FiveVsFive = 1,
+
+        [Display(Name = "6/6")]
+        SixVsSix = 2,
+
+        [Display(Name = "7/7")]
+        SevenVsSeven = 3,
+
+        [Display(Name = "8/8")]
+        EightVsEight = 4,
+
+        [Display(Name = "9/9")]
+        NineVsNine = 5,
+
+        [Display(Name = "10/10")]
+        TenVsTen = 6,
+
+        [Display(Name = "11/11")]
+        ElevenVsEleven = 7,
+
+        Other = 8,
+    }
+}
