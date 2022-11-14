d0759172d76a9be79290b09f8b5ce9f56bdec412
Mihail <mishocara@gmail.com>
2022-11-14 12:34:11 +0200

    Add FieldStatus Enumeration

---
+++ Data/LetsPlayFootball.Data.Models/Fields/FieldStatus.cs
@@ -0,0 +1,13 @@
+﻿namespace LetsPlayFootball.Data.Models.Fields
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public enum FieldStatus
+    {
+        Reserved = 1,
+        Available = 2,
+
+        [Display(Name = "Temporary Unavailable")]
+        TemporatyUnavailable = 3,
+    }
+}
