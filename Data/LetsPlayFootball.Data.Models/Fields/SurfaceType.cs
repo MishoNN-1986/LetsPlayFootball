db9287223cf6fbbe37170e0f5b7c0936e8e75add
Mihail <mishocara@gmail.com>
2022-11-14 13:14:34 +0200

    Add SurfaceType Enumeration

---
+++ Data/LetsPlayFootball.Data.Models/Fields/SurfaceType.cs
@@ -0,0 +1,17 @@
+﻿namespace LetsPlayFootball.Data.Models.Fields
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public enum SurfaceType
+    {
+        Grass = 1,
+
+        [Display(Name = "Artificial Grass")]
+        ArtificialGrass = 2,
+
+        Wooden = 3,
+        Asphalt = 4,
+        Rubber = 5,
+        Other = 6,
+    }
+}
