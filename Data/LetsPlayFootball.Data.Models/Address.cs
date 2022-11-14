654e218dd4bad2dc27382069fbc1e5725d7c77a6
Mihail <mishocara@gmail.com>
2022-11-14 18:51:45 +0200

    Add navigation property to Address Db Model

--- Data/LetsPlayFootball.Data.Models/Address.cs
+++ Data/LetsPlayFootball.Data.Models/Address.cs
@@ -1,9 +1,11 @@
 ﻿namespace LetsPlayFootball.Data.Models
 {
+    using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
 
     using LetsPlayFootball.Data.Common.Models;
     using LetsPlayFootball.Data.Models.Arenas;
+    using LetsPlayFootball.Data.Models.Users;
 
     public class Address : BaseDeletableModel<int>
     {
@@ -16,5 +18,7 @@
         public int CityId { get; set; }
 
         public virtual City City { get; set; }
+
+        public virtual ICollection<ApplicationUser> Users { get; set; } = new HashSet<ApplicationUser>();
     }
 }
