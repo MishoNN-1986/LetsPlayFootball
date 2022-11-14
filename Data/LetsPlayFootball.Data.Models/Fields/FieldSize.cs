db2da28960f335c191d538c3468c59e2d12d21fe
Mihail <mishocara@gmail.com>
2022-11-14 11:23:01 +0200

    Add FieldSize Db Model

---
+++ Data/LetsPlayFootball.Data.Models/Fields/FieldSize.cs
@@ -0,0 +1,15 @@
+﻿namespace LetsPlayFootball.Data.Models.Fields
+{
+    using LetsPlayFootball.Data.Common.Models;
+
+    public class FieldSize : BaseModel<int>
+    {
+        public int Length { get; set; }
+
+        public int Width { get; set; }
+
+        public int FieldId { get; set; }
+
+        public virtual Field Field { get; set; }
+    }
+}
