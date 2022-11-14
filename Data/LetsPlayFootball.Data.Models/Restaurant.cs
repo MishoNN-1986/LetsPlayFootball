0f9adf71a1636d634035d6ed92c207a3e1cdf3d5
Mihail <mishocara@gmail.com>
2022-11-14 10:08:52 +0200

    Add Restaurant Db Model

---
+++ Data/LetsPlayFootball.Data.Models/Restaurant.cs
@@ -0,0 +1,19 @@
+﻿namespace LetsPlayFootball.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using LetsPlayFootball.Data.Common.Models;
+
+    public class Restaurant : BaseDeletableModel<int>
+    {
+        [MaxLength(15)]
+        public int PhoneNumber { get; set; }
+
+        [MaxLength(260)]
+        public string WebUrl { get; set; }
+
+        public int FeaturesId { get; set; }
+
+        public virtual Features Features { get; set; }
+    }
+}
