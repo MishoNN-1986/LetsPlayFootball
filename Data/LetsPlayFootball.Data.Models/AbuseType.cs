3534c6cedabbfadd8f98a1a4b54ee184a98941b1
Mihail <mishocara@gmail.com>
2022-11-14 14:41:53 +0200

    Add AbuseType Enumeration

---
+++ Data/LetsPlayFootball.Data.Models/AbuseType.cs
@@ -0,0 +1,21 @@
+﻿namespace LetsSport.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public enum AbuseType
+    {
+        [Display(Name = "Physical Aggression")]
+        PhysicalAggression = 1,
+
+        [Display(Name = "Sexual Harisement")]
+        SexualHarisement = 2,
+
+        [Display(Name = "Aggressive Language")]
+        AggressiveLanguage = 3,
+
+        [Display(Name = "Bad Language")]
+        BadLanguage = 4,
+
+        Spam = 5,
+    }
+}
