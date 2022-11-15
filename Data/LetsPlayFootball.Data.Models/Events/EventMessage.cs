22dcdd17a06513cad6b6c9a18e945da15da82395
Mihail <mishocara@gmail.com>
2022-11-15 16:25:43 +0200

    Add EventMessage Db Model

---
+++ Data/LetsPlayFootball.Data.Models/Events/EventMessage.cs
@@ -0,0 +1,29 @@
+﻿namespace LetsPlayFootball.Data.Models.Events
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    using LetsPlayFootball.Data.Common.Models;
+    using LetsPlayFootball.Data.Models.Users;
+
+    public class EventMessage : BaseModel<string>
+    {
+        public EventMessage()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public string SenderId { get; set; }
+
+        public virtual ApplicationUser Sender { get; set; }
+
+        [Required]
+        public int EventId { get; set; }
+
+        public virtual Event Event { get; set; }
+    }
+}
