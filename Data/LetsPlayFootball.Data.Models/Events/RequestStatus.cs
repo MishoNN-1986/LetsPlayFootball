e7fc779ef6696ad3462b591001a032a1ba8fe917
Mihail <mishocara@gmail.com>
2022-11-15 16:17:09 +0200

    Add RequestStatus Enumeration

---
+++ Data/LetsPlayFootball.Data.Models/Events/RequestStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace LetsPlayFootball.Data.Models.Events
+{
+    public enum RequestStatus
+    {
+        Approved = 1,
+        Denied = 2,
+        Pending = 3,
+        Canceled = 4,
+    }
+}
