1cd9f9980280d198fd6e34e93e62410daa53c6cf
Mihail <mishocara@gmail.com>
2022-11-15 18:37:26 +0200

    Add PlayerDemand Db Model

---
+++ Data/LetsPlayFootball.Data.Models/Events/PlayerDemand.cs
@@ -0,0 +1,17 @@
+﻿namespace LetsPlayFootball.Data.Models.Events
+{
+    using System.Collections.Generic;
+
+    using LetsPlayFootball.Data.Common.Models;
+
+    public class PlayerDemand : BaseModel<int>
+    {
+        public int NeededPlayers { get; set; }
+
+        public int EventId { get; set; }
+
+        public virtual Event Event { get; set; }
+
+        public virtual ICollection<PlayerJoinRequest> PlayerJoinRequests { get; set; } = new HashSet<PlayerJoinRequest>();
+    }
+}
