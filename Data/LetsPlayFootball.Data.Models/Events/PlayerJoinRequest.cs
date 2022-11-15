3fbe1f53808c3ab2019b3bd5e4e3c928708bf480
Mihail <mishocara@gmail.com>
2022-11-15 17:32:47 +0200

    Add PlayerJoinRequest Db Model

---
+++ Data/LetsPlayFootball.Data.Models/Events/PlayerJoinRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace LetsPlayFootball.Data.Models.Events
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using LetsPlayFootball.Data.Common.Models;
+    using LetsPlayFootball.Data.Models.Users;
+
+    public class PlayerJoinRequest : BaseModel<int>
+    {
+        [Required]
+        public int JoinedPlayers { get; set; }
+
+        public virtual RequestStatus RequestStatus { get; set; }
+
+        public int UserId { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+    }
+}
