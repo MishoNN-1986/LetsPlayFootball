4f208afc91de402f6de7ef07c7778668d9f1cea9
Mihail <mishocara@gmail.com>
2022-11-15 16:44:27 +0200

    Add PrivateMessage Db Model

---
+++ Data/LetsPlayFootball.Data.Models/Users/PrivateMessage.cs
@@ -0,0 +1,28 @@
+﻿namespace LetsPlayFootball.Data.Models.Users
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    using LetsPlayFootball.Data.Common.Models;
+
+    public class PrivateMessage : BaseModel<string>
+    {
+        public PrivateMessage()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public string SenderId { get; set; }
+
+        public virtual ApplicationUser Sender { get; set; }
+
+        [Required]
+        public string RecipientId { get; set; }
+
+        public virtual ApplicationUser Recipient { get; set; }
+    }
+}
